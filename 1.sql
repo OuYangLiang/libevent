ac5dd5f3b08599ede908c39793b79ce88d988061
OuYang Liang <ouyanggod@gmail.com>
2018-10-03 22:10:09 +0800

    增加事件历史表，方便消费端实现幂等。

--- 1.sql
+++ 1.sql
@@ -77,3 +77,14 @@ create table if not exists `event_queue_7` (
   primary key (`id`),
   unique key(`event_id`)
 ) engine=innodb default charset=utf8 comment='事件队列表';
+
+create table if not exists `event_history` (
+  `id`              bigint      not null auto_increment comment '主键',
+  `event_id`        char(32)    not null comment '事件id',
+  `subscriber_id`   varchar(12) not null comment '订阅者id',
+  `event_type`      char(12)    not null comment '事件类型',
+  `event_time`      datetime    not null comment '事件发生时间',
+  `context`         mediumtext  not null comment '事件内容',
+  primary key (`id`),
+  unique key(`event_id`, `subscriber_id`)
+) engine=innodb default charset=utf8 comment='事件历史表';
