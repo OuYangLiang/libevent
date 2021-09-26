4f418416ee3ddb96dadbc722b8053c2c7932ffc5
OuYangLiang <ouyanggod@gmail.com>
2021-09-26 16:24:36 +0800

    Fix a fatal issue, the system.exit is used to catch the exception, which cause the jvm process to exit.

--- libevent-rocketmq/src/main/java/com/personal/oyl/event/rocketmq/RocketMqEventPusher.java
+++ libevent-rocketmq/src/main/java/com/personal/oyl/event/rocketmq/RocketMqEventPusher.java
@@ -34,7 +34,6 @@ public class RocketMqEventPusher implements EventPusher {
             this.producer.start();
         } catch (MQClientException e) {
             log.error(e.getMessage(), e);
-            System.exit(1);
         }
     }
 
